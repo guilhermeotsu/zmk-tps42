f18fc3a196c708276cd5323f6f5c56d235b5c7af
Hunter Wei <hunterwei@gmail.com>
2024-02-27 00:33:05 -0600

    3nd test of the re-position pivot x, y values

--- boards/shields/nice_view_inverted/custom_status_screen.c
+++ boards/shields/nice_view_inverted/custom_status_screen.c
@@ -23,8 +23,8 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_status_init(&status_widget, screen);
     lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
     // Rotate the screen object 180 degree
-    lv_obj_set_style_transform_pivot_x(screen, 34, 0);
-    lv_obj_set_style_transform_pivot_y(screen, 80, 0);
+    lv_obj_set_style_transform_pivot_x(screen, 80, 0);
+    lv_obj_set_style_transform_pivot_y(screen, 34, 0);
     lv_obj_set_style_transform_angle(screen, 1800, 0);
 #endif
 
