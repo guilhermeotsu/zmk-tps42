d27f60aee5af313be707ee6a3ed16a6875069624
Hunter Wei <hunterwei@gmail.com>
2024-02-27 00:00:04 -0600

    Refactored syntax for lv_obj_set_style_transform_angle().

--- boards/shields/nice_view_inverted/custom_status_screen.c
+++ boards/shields/nice_view_inverted/custom_status_screen.c
@@ -22,7 +22,7 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_status_init(&status_widget, screen);
     lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_display_set_rotation(screen, LV_DISPLAY_ROTATION_180);
+    lv_obj_set_style_transform_angle(screen, 1800, 0)
 #endif
 
     return screen;
